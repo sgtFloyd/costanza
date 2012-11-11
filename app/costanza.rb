b37c3f6b8ebb799116418c90d53ee393ad42c3a6
mrb <michaelrbernstein@gmail.com>
2012-11-10 20:31:33 -0500

    allow direct linking

--- app/costanza.rb
+++ app/costanza.rb
@@ -7,10 +7,23 @@ get '/' do
 end
 
 get '/.gif' do
-  send_file gifs.sample
+  send_file random_gif
+end
+
+get '/:num.gif' do
+  send_file direct_gif(params[:num])
 end
 
 def gifs
   Dir[File.join('public', 'img', '*.gif')]
     .select{|f| !f.match(/progress\.gif/)}
+end
+
+def random_gif
+  gifs.sample
+end
+
+def direct_gif(num)
+  gif_file = File.join('public', 'img', "#{num}.gif")
+  File.exists?(gif_file) ? gif_file : random_gif
 end
